9de24950ba59a3f9c9f7a6ffc4bb30389912413a
lejesterdelacorte <lejesterdelacorte@gmail.com>
2023-02-16 10:49:45 -0300

    third commit

--- clima/index.php
+++ clima/index.php
@@ -1,23 +1,25 @@
 <?php
   require_once('./services/ip-api.php');
   require_once('./services/weather-api.php');
+  $archivo = __DIR__.'/config.ini';
+  $content = parse_ini_file($archivo, true);
+  
+      
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="styles.css"></link>
 <title>Weather data</title>
 </head>
 
 <body>
-<h1>
-  <a href="index.php" style="color:#fff;text-decoration:none">php<span class="color">Scaffold</span></a>
-</h1>
-
+<div class='header' ><h1>CONSULTA A API DEL CLIMA</h1></div>
 <div class="container">
-  <h1> IP: <?= getIp(); ?> </h1>
-  <h1> TEMP: <?= getTemp(WEATHERAPI.API_KEY, WEATHERAPI.API_URL, getIp()); ?> </h1>
+  <h3> IP: <?= getIp(); ?> </h3>
+  <h3> TEMP: <?= getTemp($content['WEATHER_API']['API_KEY'], $content['WEATHER_API']['API_URL'], getIp()); ?> Celsius </h3>
 </div>
 
 </body>
--- clima/services/weather-api.php
+++ clima/services/weather-api.php
@@ -1,7 +1,7 @@
 <?php
     require_once('./services/caller.php');
     function getTemp($apikey, $apiuri, $ip) {
-        $result = CallAPI('GET', API_URL.API_KEY.'&q='.getIp().'&aqi=no');
+        $result = CallAPI('GET', $apiuri.$apikey.'&q='.getIp().'&aqi=no&iconSet=icons1');
         return $result->current->temp_c;
     }
 ?>
